c395d2eae2f46df12c63ccbba517935b82ef4f51
al-jaber-nishad <mailnishad02@gmail.com>
2021-12-05 15:09:54 +0600

    Registration functionality added

--- registration/forms.py
+++ registration/forms.py
@@ -3,9 +3,16 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class UserRegisterForm(UserCreationForm):
-  email = forms.EmailField()
+  username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}), label="Userame")
+  first_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}), label="First Name")
+  last_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}), label="Last Name")
+  email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}), label="Email")
+  password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}), label="Password")
+  password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}), label="Password Confirmation")
+
 
   class Meta:
     model = User
 
-    fields = ['username', 'email', 'password1', 'password2']
+    fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
--- registration/urls.py
+++ registration/urls.py
@@ -1,5 +1,16 @@
 from django.urls import path 
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views 
+
+
 urlpatterns = [
-  path('register/', views.register, name="register")
+  path('register/', views.register, name="register"),
+  path('profile/', views.profile, name="profile"),
+  path('login/', auth_views.LoginView.as_view(), name='login'),
+  path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
+
+if settings.DEBUG:
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
