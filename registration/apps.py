3d15d6f16f8e82e81485e8406e98afabe9c02f15
al-jaber-nishad <mailnishad02@gmail.com>
2022-01-22 20:04:35 +0600

    Update

--- registration/apps.py
+++ registration/apps.py
@@ -5,5 +5,5 @@ class RegistrationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'registration'
 
-    def ready(self):
-        import registration.signals
+    # def ready(self):
+    #     import registration.signals
