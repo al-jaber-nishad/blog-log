7bc7140e0570e5ea7d9823307158084b1fc6c358
al-jaber-nishad <mailnishad02@gmail.com>
2022-02-17 20:35:18 +0600

    Update

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils.timezone import now, localtime
 from ckeditor.fields import RichTextField
+from PIL import Image
 
 # Create your models here.
 
@@ -20,3 +21,13 @@ class Post(models.Model):
 
   def get_absolute_url(self):
     return reverse('post-detail', kwargs={'pk': self.pk})
+
+  # To change the resolution of the uploaded picture
+  def save(self, *args, **kwargs):
+    super().save(*args, **kwargs)
+
+    img = Image.open(self.thumbnail_image.path)
+
+    if img.height>300 or img.width>300:
+      img.thumbnail((300, 300))
+      img.save(self.image.path)
--- registration/models.py
+++ registration/models.py
@@ -6,7 +6,7 @@ from PIL import Image
 
 class Profile(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
-
+  bio = models.CharField(max_length=200)
   image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
   def __str__(self):
